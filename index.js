8bcc73bedb7472ccf4017e0b58b593d24759a7a2
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-18 18:33:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,5 +21,12 @@ function keepGoing(array, changeValue, skipValue) {
 }
 
 function findBy(array, findFn) {
-
+  for (let i = 0; i < array.length; i++) {
+    if (findFn(array[i])) {
+      return array[i];
+    } else {
+      array[i] = changeValue;
+    }
+  }
+  return array;
 }
