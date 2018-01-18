eef2f010c6395f89baf593ed002c0018b9ee4b93
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-18 18:36:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ function keepGoing(array, changeValue, skipValue) {
 
 function findBy(array, findFn) {
   for (let i = 0; i < array.length; i++) {
+    console.log(findFn(array[i]))
     if (findFn(array[i])) {
       return array[i];
     } else {
